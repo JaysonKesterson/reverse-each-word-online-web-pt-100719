2fd88043714df6906f3b6fe5995920a102d19e85
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-10-14 22:03:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,11 +8,3 @@ def reverse_each_word(string)
   new_array.join(" ")
 end
 
-def reverse_each_word(string)
-  array = string.split
-  
-  array.collect do |element|
-      element.reverse
-  end
-  array.join("")
-end
